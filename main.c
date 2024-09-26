37187ae1e84450b4aa878b3eff08a5712f98d80b
Atakan Celebi <atakancelebi2007@gmail.com>
2024-09-26 22:32:52 +0200

    modi

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-    float a;
-    printf("inserisci il prezzo totale");
+    float a=0;
+    printf("inserisci il prezzo totale\n");
     scanf("%f", &a);
-    if (a > 10) {
-        printf("importo non scontato e' %f \n:", a);
-        a = a-(a*0.2);
-        printf("importo scontato e' %f: \n", a);
-    } else if ("a > 50") {
-        printf("importo non scontato e' %f:\n", a);
-        a = a-(a*0.1);
-        printf("importo scontato e' %f:\n", a);
+    if (a > 10 && a <= 50) {
+        printf("importo non scontato e' %.2f: \n", a);
+        a -= (a * 0.1);
+        printf("importo scontato e' %.2f: \n", a);
+    } else if (a > 50) {
+        printf("importo non scontato e' %.2f: \n", a);
+        a -= (a * 0.2);
+        printf("importo scontato e' %.2f: \n", a);
     } else {
         printf("il prezzo e' troppo basso per essere scontato");
     }
